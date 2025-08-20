e505235e3f6686e4e41a3863cef29aedfd3243f7
최은희 <polarisinuniverse@choeeunhuiui-iMac.local>
2025-08-20 15:34:06 +0900

    feat: 폰트 및 theme 추가

---
+++ lib/theme/theme.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  // 폰트 사이즈
+  static const double headerFontSize = 24.0;
+  static const double titleFontSize = 16.0;
+  static const double bodyFontSize = 12.0;
+
+  // 폰트 굵기
+  static const FontWeight headerFontWeight = FontWeight.w900;
+  static const FontWeight titleFontWeight = FontWeight.w600;
+  static const FontWeight bodyFontWeight = FontWeight.w400;
+
+  // 텍스트 스타일
+  static const TextStyle headerStyle = TextStyle(
+    fontSize: headerFontSize,
+    fontWeight: headerFontWeight,
+  );
+
+  static const TextStyle titleStyle = TextStyle(
+    fontSize: titleFontSize,
+    fontWeight: titleFontWeight,
+  );
+
+  static const TextStyle bodyStyle = TextStyle(
+    fontSize: bodyFontSize,
+    fontWeight: bodyFontWeight,
+  );
+
+  static ThemeData DarkTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: 'Paperlogy',
+    colorScheme: const ColorScheme.dark(
+      primary: Colors.deepPurpleAccent,
+      onPrimary: Colors.white,
+      onSurface: Colors.white,
+    ),
+  );
+}
